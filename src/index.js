508d0601b05fea9641da4f2e514d6ed93cbc4278
Raquel Gruppi <raquel.gruppi@gmail.com>
2021-11-22 22:14:29 +1100

    Challenge finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(_user => _user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Free todos ended'})
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id is not uuid' });
+  }
+  
+  const user = users.find(_user => _user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found' });
+  }
+  
+  const todo =  user.todos.find(_todo => _todo.id === id);
+   
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+  
+  request.user = user;
+  request.todo = todo;
+  
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(_user => _user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
